5f864e644c9d945cacf2f4ccae729535577b7009
bl2i4n <bl2i4n@yahoo.com>
2017-07-08 15:43:22 -0700

    fixing sendgrid

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -6,7 +6,7 @@ if Rails.env.development? || Rails.env.production?
       authentication: :plain,
       user_name:      ENV['SENDGRID_USERNAME'],
       password:       ENV['SENDGRID_PASSWORD'],
-      domain:         'heroku.com',
+      domain:         'savegame.herokuapp.com',
       enable_starttls_auto: true
     }
 end
