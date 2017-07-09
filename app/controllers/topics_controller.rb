9b04283fb8d3ff42a90510cdb91335760b0d99d5
bl2i4n <bl2i4n@yahoo.com>
2017-07-08 19:35:06 -0700

    creating topics and viewing topics now work

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,9 +4,24 @@ class TopicsController < ApplicationController
   end
 
   def show
+    @topic = Topic.find(params[:id])
   end
 
   def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new
+    @topic.title = params[:topic][:title]
+
+    if @topic.save
+      flash[:notice] = "Topic was saved."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "There was an error saving the topic. Please try again."
+      render :new
+    end
   end
 
   def edit
