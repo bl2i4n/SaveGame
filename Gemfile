02203859921b80f20adfde728aff97d47fc8fa87
bl2i4n <bl2i4n@yahoo.com>
2017-07-15 12:56:28 -0700

    added edit bookmark button and need to add delete bookmark button

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'pundit'
 gem 'jquery-rails'
 gem 'devise'
 gem 'bootstrap-sass'
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,5 @@
 class BookmarksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_topic
   before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
 
