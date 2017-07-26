18006f9a69d0389f5b4ec0faef87a2e63ddb9494
bl2i4n <bl2i4n@yahoo.com>
2017-07-25 19:18:56 -0700

    likes controller needs more work

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,27 @@
 class LikesController < ApplicationController
   def index
   end
+
+  def create
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.build(bookmark: @bookmark)
+
+    if like.save
+      #need code
+    else
+      #need moar code
+    end
+  end
+
+  def destroy
+    # Get the bookmark from the params
+    # Find the current user's like with the ID in the params
+
+    if like.destroy
+      # Flash success and redirect to @bookmark
+    else
+      # Flash error and redirect to @bookmark
+    end
+  end
+
 end
