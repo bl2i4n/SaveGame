e5da52d662beeada4de8b3bccd7e5370cce8d7ca
bl2i4n <bl2i4n@yahoo.com>
2017-07-27 15:44:44 -0700

    added like_policy for likes but unable to view bookmark or like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user
+
   def index
   end
 
@@ -7,21 +9,27 @@ class LikesController < ApplicationController
     like = current_user.likes.build(bookmark: @bookmark)
 
     if like.save
-      #need code
+      flash[:notice] = "Bookmark Liked!"
     else
-      #need moar code
+      flash[:alert] = "Like failed"
     end
+    redirect_to [topic.bookmark. bookmark]
   end
 
   def destroy
     # Get the bookmark from the params
     # Find the current user's like with the ID in the params
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.find(params[:id])
 
     if like.destroy
       # Flash success and redirect to @bookmark
+      flash[:notice] = "Bookmark unfavorited."
     else
       # Flash error and redirect to @bookmark
+      flash[:alert] = "Unfavoriting failed."
     end
+      redirect_to [topic.bookmark. bookmark]
   end
 
 end
