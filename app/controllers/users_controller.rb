361f6ebe46a098a576d9e9e3db857f511bcda9c2
bl2i4n <bl2i4n@yahoo.com>
2017-07-27 22:51:03 -0700

    added index and show controllers actions for user prof, also added view details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+  before_action :require_sign_in
+
   def show
+    if current_user
+      @user = params[:id] ? User.find(params[:id]) : current_user
+      @topics = @user.topics
+      @liked_bookmarks = @user.likes.map(&:bookmark)
+    else
+      redirect_to new_user_registration_path
+    end
   end
+
+  def new
+  end
+
+  def index
+    @user = User.find(params[:id])
+    @bookmarks = @user.topic.bookmarks
+  end
+
 end
